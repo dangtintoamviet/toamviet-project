c40ebba4c3ce0393056f914a908b173a025ae90e
dangtintoamviet <dangtin.toamviet@gmail.com>
2026-03-15 20:19:33 +0700

    Update component-loader.js

--- assets/js/component-loader.js
+++ assets/js/component-loader.js
@@ -1,1 +1,23 @@
+async function loadComponent(id, file) {
+  try {
+    const response = await fetch(file);
+    const data = await response.text();
+    document.getElementById(id).innerHTML = data;
+  } catch (error) {
+    console.error("Không load được component:", file);
+  }
+}
 
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.getElementById("site-header")) {
+    loadComponent("site-header", "./components/header.html");
+  }
+
+  if (document.getElementById("site-footer")) {
+    loadComponent("site-footer", "./components/footer.html");
+  }
+
+  if (document.getElementById("site-mobile-menu")) {
+    loadComponent("site-mobile-menu", "./components/mobile-menu.html");
+  }
+});
